fc5f6467ac0923ec8e78710d8a622cb80c0d6735
Joshua Lawrinenko <jrlawrinenko@gmail.com>
2023-01-03 12:43:26 -0600

    added features

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,28 +1,33 @@
 package app;
 
+import java.io.File;
+import java.io.FileWriter;
+
 /**
- * Simple math app.
+ * MathNum demo app
  * @author Joshua Lawrinenko
  */
 public class Main 
 {
 	/**
-	 * Scanner object
+	 * Scanner
 	 */
 	public static java.util.Scanner in = new java.util.Scanner(System.in);
-	
+
 	/**
-	 * Performs MathNum operations.
-	 * @param args cmd line args
-	 * @throws java.io.IOException from Scanner
+	 * Test function 0
 	 */
-	public static void main( String[] args ) throws java.io.IOException {
-		int a, b, c;
+	public static void test0() {
+		long a, b;
+		int c, d, e;
 		
-		System.out.print("Enter 2 numbers and a desired precision: ");
-		a = in.nextInt();
-		b = in.nextInt();
+		System.out.println();
+		System.out.print("Enter 2 numbers, desired precision, and 2 powers.\nFormat <long long int int int>: ");
+		a = in.nextLong();
+		b = in.nextLong();
 		c = in.nextInt();
+		d = in.nextInt();
+		e = in.nextInt();
 		MathNum num1 = new MathNum(a);
 		MathNum num2 = new MathNum(b);
 		MathNum.setDefPrec(c);
@@ -32,5 +37,68 @@ public class Main
 		System.out.println("LCM of : " + num1 + ", " + num2 + " = " + num1.lcm(num2));
 		System.out.println("Sqrt of : " + num1 + " = " + num1.sqrt());
 		System.out.println("Sqrt of : " + num2 + " = " + num2.sqrt());
+		System.out.println("Factors of : " + num1 + " : " + num1.getFactors());
+		System.out.println("Factors of : " + num2 + " : " + num2.getFactors());
+		System.out.println("Prime factors of : " + num1 + " : " + num1.getPrimeFactors());
+		System.out.println("Prime factors of : " + num2 + " : " + num2.getPrimeFactors());
+		System.out.println("POW : " + num1 + "^" + Integer.toString(d) + " = " + num1.pow(d));
+		System.out.println("POW : " + num2 + "^" + Integer.toString(e) + " = " + num2.pow(e));
+		System.out.println();
+	}
+
+	/**
+	 * Test function 1
+	 * @param a long
+	 * @param b long
+	 * @param c int
+	 * @param d int
+	 * @param e int
+	 */
+	public static void test1(long a, long b, int c, int d, int e) {
+		MathNum num1 = new MathNum(a);
+		MathNum num2 = new MathNum(b);
+		MathNum.setDefPrec(c);
+
+		System.out.println();
+		System.out.println("GCD of : " + num1 + ", " + num2 + " = " + num1.gcd(num2));
+		System.out.println("LCM of : " + num1 + ", " + num2 + " = " + num1.lcm(num2));
+		System.out.println("Sqrt of : " + num1 + " = " + num1.sqrt());
+		System.out.println("Sqrt of : " + num2 + " = " + num2.sqrt());
+		System.out.println("Factors of : " + num1 + " : " + num1.getFactors());
+		System.out.println("Factors of : " + num2 + " : " + num2.getFactors());
+		System.out.println("Prime factors of : " + num1 + " : " + num1.getPrimeFactors());
+		System.out.println("Prime factors of : " + num2 + " : " + num2.getPrimeFactors());
+		System.out.println("POW : " + num1 + "^" + Integer.toString(d) + " = " + num1.pow(d));
+		System.out.println("POW : " + num2 + "^" + Integer.toString(e) + " = " + num2.pow(e));
+		System.out.println();
+	}
+
+	/**
+	 * Outputs primes up to x in file "primes.txt"
+	 * @param x integer
+	 */
+	public static void test2(int x) {
+		if (x < 1 || x > 2_000_000) return;
+		try {
+			File f = new File("primes.txt");
+			FileWriter fw = new FileWriter(f);
+			for (long i = 1; i < x; i += 2) {
+				if (MathNum.isPrime(i)) fw.write(Long.toString(i) + ",\n");
+			}
+			fw.close();
+		} catch(java.io.IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	/**
+	 * Demos MathNum operations
+	 * @param args args
+	 * @throws java.io.IOException from Scanner
+	 */
+	public static void main(String[] args) throws java.io.IOException {
+		test0();
+		// test1(9_120_120_120L, 4_000_000_500L, 20, 0, 0);
+		// test1(1_327_000_327L, 7_327_327_327L, 20, 0, 0);
 	}
 }
--- src/main/java/app/MathNum.java
+++ src/main/java/app/MathNum.java
@@ -6,78 +6,190 @@ package app;
  */
 public class MathNum {
 	private static int _def_prec = 12;
-	private int _val;
+	private long _val;
 	private int _prec;
+	private java.util.ArrayList<Long> _flist;
+	private java.util.ArrayList<Long> _pflist;
 
-	/**
-	 * Use Integer.MIN_VALUE, static precision
-	 */
-	public MathNum() {
-		_val = Integer.MIN_VALUE;
-		_prec = _def_prec;
+	private void setFactorList() {
+		for (long i = 1; i * i < _val + 1; i++) {
+			if (_val % i == 0) _flist.add(i);
+		}
+		int lim = _flist.size() - 1;
+		for (int i = lim; i >= 0; i--) {
+			_flist.add((long) _val / _flist.get(i));
+		}
+	}
+
+	private void setPrimeFactorList() {
+		long temp = _val;
+		for (long f : _flist) {
+			if (MathNum.isPrime(f)) {
+				while (temp % f == 0) { 
+					_pflist.add(f);
+					temp = temp / f;
+				}
+			}
+		}
 	}
 
 	/**
-	 * Use given number, static precision
-	 * @param n number
+	 * Determines primality of given number
+	 * @param x long
+	 * @return boolean
 	 */
-	public MathNum(int n) {
-		_val = n;
-		_prec = _def_prec;
-	}
-
-	/**
-	 * Use given number, given precision
-	 * @param n number
-	 * @param p precision
-	 */
-	public MathNum(int n, int p) {
-		_val = n;
-		_prec = p;
+	public static boolean isPrime(long x) {
+		if (x <= 1) return false;
+		if (x <= 3) return true;
+		if (x % 2 == 0) return false;
+		for (long i = 5; i * i < x; i += 2) {
+			if (x % i == 0) return false;
+		}
+		return true;
 	}
 
 	/**
 	 * Set static precision
-	 * @param p precision
+	 * @param p 0 &lt; p &lt; 10^3
 	 */
 	public static void setDefPrec(int p) {
-		_def_prec = p;
+		if (p < 1) _def_prec = 1;
+		else if (p > 1000) _def_prec = 1000;
+		else _def_prec = p;
+	}
+
+	/**
+	 * Use 1 for number, static precision
+	 */
+	public MathNum() {
+		_val = 1;
+		_prec = _def_prec;
+		_flist = new java.util.ArrayList<>();
+		this.setFactorList();
+		_pflist = new java.util.ArrayList<>();
+		this.setPrimeFactorList();
+	}
+
+	/**
+	 * Use given number, static precision
+	 * @param n 0 &lt; n &lt; 10^9
+	 */
+	public MathNum(long n) {
+		if (n < 1) _val = 1;
+		else if (n > 9_999_999_999L) _val = 9_999_999_999L;
+		else _val = n;
+		_prec = _def_prec;
+		_flist = new java.util.ArrayList<>();
+		this.setFactorList();
+		_pflist = new java.util.ArrayList<>();
+		this.setPrimeFactorList();
+	}
+
+	/**
+	 * Use given number, given precision
+	 * @param n 0 &lt; n &lt; 10^9
+	 * @param p 0 &lt; n &lt; 10^3
+	 */
+	public MathNum(long n, int p) {
+		if (n < 1) _val = 1;
+		else if (n > 9_999_999_999L) _val = 9_999_999_999L;
+		else _val = n;
+		if (p < 1) _def_prec = 1;
+		else if (p > 1000) _def_prec = 1000;
+		else _def_prec = p;
+		_flist = new java.util.ArrayList<>();
+		this.setFactorList();
+		_pflist = new java.util.ArrayList<>();
+		this.setPrimeFactorList();
 	}
 
 	/**
 	 * Get number value
-	 * @return number
+	 * @return long
 	 */
-	public int val() {
+	public long getVal() {
 		return _val;
 	}
 
 	/**
 	 * Set number value
-	 * @param n number
+	 * @param n 0 &lt; n &lt; 10^9
 	 */
-	public void setVal(int n) {
-		_val = n;
+	public void setVal(long n) {
+		if (n < 1) _val = 1;
+		else if (n > 9_999_999_999L) _val = 9_999_999_999L;
+		else _val = n;
+		_flist.clear();
+		this.setFactorList();
+		_pflist.clear();
+		this.setPrimeFactorList();
 	}
 
 	/**
 	 * Set instance precision
-	 * @param p precision
+	 * @param p 0 &lt; p &lt; 10^3
 	 */
 	public void setPrec(int p) {
-		_prec = p;
+		if (p < 1) _prec = 1;
+		else if (p > 1000) _prec = 1000;
+		else _prec = p;
+	}
+
+	/**
+	 * String representation of factor list
+	 * @return String
+	 */
+	public String getFactors() {
+		String ans = "";
+		for (int i = 0; i < _flist.size() - 1; i++) {
+			ans += Long.toString(_flist.get(i)) + ", ";
+		}
+		ans += Long.toString(_flist.get(_flist.size() - 1));
+		return ans;
+	}
+
+	/**
+	 * String representation of prime factor list
+	 * @return String
+	 */
+	public String getPrimeFactors() {
+		if (_pflist.size() == 1) return Long.toString(_pflist.get(0));
+		String ans = "";
+		long fct = _pflist.get(0);
+		int mult = 1;
+		for (int i = 1; i < _pflist.size() - 1; i++) {
+			if (fct != _pflist.get(i)) {
+				if (mult > 1) {
+					ans += Long.toString(fct) + "^" + Integer.toString(mult) + " * ";
+					mult = 1;
+				} else {
+					ans += Long.toString(fct) + " * ";
+				}
+				fct = _pflist.get(i);
+			} else {
+				mult++;
+			}
+		}
+		if (fct == _pflist.get(_pflist.size() - 1)) {
+			ans += Long.toString(fct) + "^" + Long.toString(mult + 1);
+		} else if (mult > 1) {
+			ans += Long.toString(fct) + "^" + Long.toString(mult) + " * " + Long.toString(_pflist.get(_pflist.size() - 1));
+		} else {
+			ans += Long.toString(fct) + " * " + Long.toString(_pflist.get(_pflist.size() - 1));
+		}
+		return ans;
 	}
 
 	/**
 	 * Compute GCD of two MathNum objects
 	 * @param other MathNum object
-	 * @return integer value
+	 * @return long
 	 */
-	public int gcd(MathNum other) {
+	public long gcd(MathNum other) {
 		if (this._val == 0) return other._val;
 		if (other._val == 0) return this._val;
 		if (this._val == other._val) return this._val;
-		int a, b, c;
+		long a, b, c;
 		if (this._val > other._val) {
 			a = this._val;
 			b = other._val;
@@ -94,9 +206,9 @@ public class MathNum {
 	/**
 	 * Compute LCM of two MathNum objects
 	 * @param other MathNum object
-	 * @return integer value
+	 * @return long
 	 */
-	public int lcm(MathNum other) {
+	public long lcm(MathNum other) {
 		if (this._val > other._val)
 			return (this._val / this.gcd(other)) * other._val;
 		else
@@ -104,13 +216,52 @@ public class MathNum {
 	}
 
 	/**
-	 * Compute square root of number value
-	 * @return integer value
+	 * Compute square root of number value using instance precision
+	 * @return double
 	 */
 	public double sqrt() {
-		double ans = this._val;
+		double ans = this._val / 2.0;
 		for (int i = 0; i < _prec; i++) {
 			ans = 0.5 * (ans + (this._val / ans));
+		}
+		return ans;
+	}
+
+	/**
+	 * Compute square root of number value using given precision
+	 * @param p 0 &lt; n &lt; 10^3
+	 * @return double
+	 */
+	public double sqrt(int p) {
+		double ans = this._val / 2.0;
+		if (p < 1) p = 1;
+		else if (p > 1000) p = 1000;
+		for (int i = 0; i < p; i++) {
+			ans = 0.5 * (ans + (this._val / ans));
+		}
+		return ans;
+	}
+
+	/**
+	 * Compute number value to given power
+	 * @param x -12 &le; x &le; 12
+	 * @return double
+	 */
+	public double pow(int x) {
+		double ans = 1.0;
+		if (x == 0) return ans;
+		else if (x < -12) x = -12;
+		else if (x > 12) x = 12;
+		if (x > 0) {
+			for (int i = 0; i < x; i++) {
+				ans = ans * this._val;
+			}
+		}
+		else if (x < 0) {
+			for (int i = x; i < 0; i++) {
+				ans = ans * this._val;
+			}
+			ans = 1.0 / ans;
 		}
 		return ans;
 	}
